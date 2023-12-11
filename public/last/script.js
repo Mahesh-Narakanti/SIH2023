43d41486795a917190d8ea6f7831dd43af0d65f9
Mahesh-Narakanti <119794954+Mahesh-Narakanti@users.noreply.github.com>
2023-12-11 20:51:45 +0530

    Add files via upload

---
+++ public/last/script.js
@@ -0,0 +1,25 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Initial display
+showSlide(currentSlide);
+
+// Manual navigation with arrow buttons
+document.getElementById('prevButton').addEventListener('click', prevSlide);
+document.getElementById('nextButton').addEventListener('click', nextSlide);
